25e6171439529d62165484aa0f99490ba744ff77
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2022-02-06 19:06:02 -0500

    WOTS key pair generation

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ A MSS implementation inspired by [RFC 8391](https://datatracker.ietf.org/doc/htm
 
 Authored by Louis Tremblay Thibault and distributed under the MIT License. 
 
-
-
+---
+### Potential improvements
+- Use [WOTS+](https://eprint.iacr.org/2017/965.pdf) instead of WOTS as a signing primitive
--- src/wots.go
+++ src/wots.go
@@ -1,9 +1,50 @@
 package AMSS
 
 import (
-	"fmt"
+	"crypto/sha256"
+	"encoding/binary"
+	"math"
+	"math/rand"
+	"time"
 )
 
+/**
+Winternitz one-time signature scheme parameters
+*/
+const n = 32 // Size (in bytes) of the message to sign
+const w = 16 // Winternitz parameter
+const t = 18 // computed in function of n and w
+var signature_key [t][n]byte
+var public_key [t][n]byte
+
 func main() {
-	fmt.Println("gm!")
+	sk_init()
+	pk_init()
+}
+
+/*
+Initializes the signature (secret) key with t n-byte random string
+*/
+func sk_init() {
+	rand.Seed(time.Now().UnixNano())
+	var rvalue uint64
+
+	for i := 0; i < t; i++ {
+		for j := 0; j < n; j += 8 {
+			rvalue = rand.Uint64()
+			b := make([]byte, 8)
+			binary.LittleEndian.PutUint64(b, rvalue)
+			for k := 0; k < 8; k++ {
+				signature_key[i][j+k] = b[k]
+			}
+		}
+	}
+}
+
+func pk_init() {
+	for i := 0; i < t; i++ {
+		for j := 0; j < int(math.Pow(2, w)-1); j++ {
+			public_key[i] = sha256.Sum256(signature_key[i][:])
+		}
+	}
 }
